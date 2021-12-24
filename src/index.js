0f7432cf65750575edf8be020169cf3119dcfdb0
RATDOTL <78393301+RATDOTL@users.noreply.github.com>
2021-12-24 16:09:24 +0900

    [todoの追加]機能の実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //liタグ生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  //liタグの小要素の各要素を設定
+  li.appendChild(div);
+
+  //divタグの小要素の各要素を設定
+  div.appendChild(p);
+  console.log(li);
+  //未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(li);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
